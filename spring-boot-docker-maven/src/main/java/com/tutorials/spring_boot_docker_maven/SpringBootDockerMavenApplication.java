73e864fa38fbe91052b3ec37fdaf80c2c81fcad9
harikrishna t <harikrishna.tekkam@gmail.com>
2025-05-06 16:47:30 +0530

    functionality to generate docker image using maven

---
+++ spring-boot-docker-maven/src/main/java/com/tutorials/spring_boot_docker_maven/SpringBootDockerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.spring_boot_docker_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenApplication.class, args);
+	}
+
+}
---
+++ spring-boot-docker-maven/src/test/java/com/tutorials/spring_boot_docker_maven/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.spring_boot_docker_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
